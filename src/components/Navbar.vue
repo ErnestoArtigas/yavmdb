5c5fb9fc6d83eeaec1f00bf8750cf0cc6a9701c6
Ernesto Artigas <ernart99@gmail.com>
2022-02-08 03:08:15 +0100

    draft of navbar, bug with append and prepend icon on form-input

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,47 @@
 <template>
-  <div class="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <b-navbar toggleable="lg" type="dark" id="navbar">
+    <router-link to="/"
+      ><b-navbar-brand
+        ><b-img
+          src="@/assets/logoWithText.png"
+          style="height: 2em"
+          fluid-grow
+        ></b-img></b-navbar-brand
+    ></router-link>
+
+    <b-navbar-nav class="ml-auto">
+      <b-form-input
+        v-model="searchedMovie"
+        @keyup.enter="searchMovie"
+        placeholder="Rechercher un film"
+      ></b-form-input>
+    </b-navbar-nav>
+  </b-navbar>
 </template>
 
 <script>
 export default {
   name: "Navbar",
-  props: {
-    msg: String,
+  data() {
+    return {
+      searchedMovie: "",
+      data: "<b-icon-search></b-icon-search>",
+    };
+  },
+  methods: {
+    searchMovie() {
+      console.log(this.searchedMovie);
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#navbar {
+  background-color: #005aa3 !important;
+  padding: 15px;
+}
+a {
+  text-decoration: none;
+}
+</style>
